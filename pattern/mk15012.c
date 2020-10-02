fcbd98ccd29e58a2b726c7fd8c818c5a4a781a64
Restyled.io <commits@restyled.io>
2020-10-02 20:50:27 +0000

    Restyled by clang-format

--- pattern/mk15012.c
+++ pattern/mk15012.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
-    int i, j, rows;
+int main() {
+  int i, j, rows;
 
-    printf("Enter number of rows : ");
-    scanf("%d", &rows);
+  printf("Enter number of rows : ");
+  scanf("%d", &rows);
 
-    printf("\n");
-    for(i=1; i<=rows; i++)
-    {
-        for(j=1; j<i; j++)
-        {
-            printf(" ");
-        }
-
-        for(j=1; j<=(rows*2 -(2*i-1)); j++)
-        {
-            printf("*");
-        }
-
-        printf("\n");
+  printf("\n");
+  for (i = 1; i <= rows; i++) {
+    for (j = 1; j < i; j++) {
+      printf(" ");
     }
 
-    return 0;
+    for (j = 1; j <= (rows * 2 - (2 * i - 1)); j++) {
+      printf("*");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
 }
