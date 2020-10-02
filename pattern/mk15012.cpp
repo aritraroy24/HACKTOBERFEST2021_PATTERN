57929c8f11a3e130e1984b8579d0d5479ffa72f1
Restyled.io <commits@restyled.io>
2020-10-02 20:02:32 +0000

    Restyled by astyle

--- pattern/mk15012.cpp
+++ pattern/mk15012.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cout<<"Enter the number of rows :  ";
-	cin>>n;
-	
-	int i, j, k = n;
-	cout<<"\n";
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j <= n; j++)
-		{	
-			if (j >= k)
-				cout << "* ";
-			else
-				cout << " ";
-		}
-		k--;
-		cout << "\n";
-	}	
+    int n;
+    cout<<"Enter the number of rows :  ";
+    cin>>n;
 
-	return 0;
+    int i, j, k = n;
+    cout<<"\n";
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (j >= k)
+                cout << "* ";
+            else
+                cout << " ";
+        }
+        k--;
+        cout << "\n";
+    }
+
+    return 0;
 }
 
