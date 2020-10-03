369e3a4a84e3a8a7b95721f19f93979ab780c703
Restyled.io <commits@restyled.io>
2020-10-03 06:33:31 +0000

    Restyled by black

--- pattern/hacky1997-1.py
+++ pattern/hacky1997-1.py
@@ -1,9 +1,9 @@
 rows = 5
 
-for row in range(1, rows+1):
+for row in range(1, rows + 1):
 
     for column in range(1, row + 1):
 
-        print(column, end=' ')
+        print(column, end=" ")
 
     print("")
