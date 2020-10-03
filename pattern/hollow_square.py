11e76230a8cfcdf0c2278a2d6a62ef3c9c391286
Restyled.io <commits@restyled.io>
2020-10-03 13:03:24 +0000

    Restyled by black

--- pattern/hollow_square.py
+++ pattern/hollow_square.py
@@ -1,6 +1,6 @@
-size = int(input('Size of square:'))
+size = int(input("Size of square:"))
 for i in range(size):
-    if i == 0 or i == size-1:
-        print('*'*size)
+    if i == 0 or i == size - 1:
+        print("*" * size)
     else:
-        print('*'+' '*(size-2)+'*')
+        print("*" + " " * (size - 2) + "*")
