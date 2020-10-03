07b28e4a50c2f4c888acabb214d443a07222c498
Restyled.io <commits@restyled.io>
2020-10-03 17:16:19 +0000

    Restyled by black

--- pattern/aortiz24.py
+++ pattern/aortiz24.py
@@ -6,5 +6,5 @@ for i in range(0, size):
     m = m - 1  # decrementing m after each loop
     for j in range(0, i + 1):
         # printing full Triangle pyramid using stars
-        print("* ", end=' ')
+        print("* ", end=" ")
     print(" ")
