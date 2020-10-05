ac2ae81693dcdd793a08ad1af7643913bdcdd045
Gunashree C <55580701+Guna-777@users.noreply.github.com>
2020-10-05 18:49:41 +0530

    Add files via upload

---
+++ pattern/comparision operator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 16:46:12 2020
+
+"""
+
+
+x=15
+y=6
+
+print('x>y=',x>y)
+print('x<y=',x<y)
+print('x>=y=',x>=y)
+print('x<=y=',x<=y)
+print('x!=y=',x!=y)
