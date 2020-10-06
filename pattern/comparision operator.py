691b7c3debaa2e0d70c86b1809327de3ca6e774c
Restyled.io <commits@restyled.io>
2020-10-06 05:04:09 +0000

    Restyled by autopep8

--- pattern/comparision operator.py
+++ pattern/comparision operator.py
@@ -5,11 +5,11 @@ Created on Sat Mar 21 16:46:12 2020
 """
 
 
-x=15
-y=6
+x = 15
+y = 6
 
-print('x>y=',x>y)
-print('x<y=',x<y)
-print('x>=y=',x>=y)
-print('x<=y=',x<=y)
-print('x!=y=',x!=y)
+print('x>y=', x > y)
+print('x<y=', x < y)
+print('x>=y=', x >= y)
+print('x<=y=', x <= y)
+print('x!=y=', x != y)
