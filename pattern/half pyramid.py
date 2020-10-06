a30a40454b3148c44385fa77a25f0ad4313c95bd
Restyled.io <commits@restyled.io>
2020-10-06 05:28:30 +0000

    Restyled by autopep8

--- pattern/half pyramid.py
+++ pattern/half pyramid.py
@@ -5,17 +5,18 @@ Created on Tue Oct  6 10:26:52 2020
 @author: Gunashree C
 """
 
- 
-def halfpyr(n): 
-      
-    for i in range(0, n): 
-      
-        for j in range(0, i+1): 
-          
-            print("* ",end="") 
-       
-        print("\r") 
-   
+
+def halfpyr(n):
+
+    for i in range(0, n):
+
+        for j in range(0, i+1):
+
+            print("* ", end="")
+
+        print("\r")
+
+
 n = 6
 
-halfpyr(n) 
+halfpyr(n)
