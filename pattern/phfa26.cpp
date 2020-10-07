fa8743c015e9e12320db661c79d82379569f2f58
Restyled.io <commits@restyled.io>
2020-10-07 11:44:18 +0000

    Restyled by astyle

--- pattern/phfa26.cpp
+++ pattern/phfa26.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int size;
     int spaces;
     cout << "Enter the your christmas three size: ";
     cin >> size;
 
-    for(int i = size; i >= 0; i--){
+    for(int i = size; i >= 0; i--) {
 
         if(size % 2 == 0)
             spaces = i/2;
         else
             spaces = i/2;
-            spaces++;
-        
-        for(int j = 0; j < spaces; j++){
+        spaces++;
+
+        for(int j = 0; j < spaces; j++) {
             cout << "  ";
         }
-        for(int j = i; j < size; j++){
+        for(int j = i; j < size; j++) {
             cout << "* ";
         }
         cout << endl;
     }
 
     if(size > 5)
-        for(int i = 0; i < size/2; i++){
+        for(int i = 0; i < size/2; i++) {
             for(int j = 0; j < size/2; j++)
                 cout << "  ";
             cout << "***" << endl;
         }
     else
-        for(int i = 0; i < size/2; i++){
+        for(int i = 0; i < size/2; i++) {
             for(int j = 0; j < size/2; j++)
                 cout << "  ";
             cout << "*" << endl;
-            }
+        }
 
     for(int i = 0; i < size*2; i++)
         cout << "*";
-        
+
     cout << endl;
 
     return 0;
