1f92497836042bd613a727290adacde40a183df5
Restyled.io <commits@restyled.io>
2020-10-13 17:31:23 +0000

    Restyled by clang-format

--- pattern/pyramid.js
+++ pattern/pyramid.js
@@ -1,25 +1,24 @@
 let fullPyramid = (rows) => {
-  
-    for(let i = 1; i <= rows; i++){
-      let str = '';
-      
-      //Add the white space to the left
-      for(let k = 1; k <= (rows - i); k++){
-        str += ' ';
-      }
-      
-      //Add the '*' for each row
-      for(let j = 0; j != (2 * i - 1); j++){
-        str += '*';
-      }
-      
-      //Add the white space to the right
-      for(let k =  i + 1; k <= rows; k++){
-        str += ' ';
-      }
-      
-       //Print the pyramid pattern for each row
-      console.log(str);
+  for (let i = 1; i <= rows; i++) {
+    let str = '';
+
+    // Add the white space to the left
+    for (let k = 1; k <= (rows - i); k++) {
+      str += ' ';
     }
- };
- fullPyramid(5);
+
+    // Add the '*' for each row
+    for (let j = 0; j != (2 * i - 1); j++) {
+      str += '*';
+    }
+
+    // Add the white space to the right
+    for (let k = i + 1; k <= rows; k++) {
+      str += ' ';
+    }
+
+    // Print the pyramid pattern for each row
+    console.log(str);
+  }
+};
+fullPyramid(5);
