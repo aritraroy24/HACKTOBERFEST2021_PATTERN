2f5dcbea51ffcd1612823001e2161615489d1332
Restyled.io <commits@restyled.io>
2020-10-13 17:31:25 +0000

    Restyled by prettier

--- pattern/pyramid.js
+++ pattern/pyramid.js
@@ -1,20 +1,20 @@
 let fullPyramid = (rows) => {
   for (let i = 1; i <= rows; i++) {
-    let str = '';
+    let str = "";
 
     // Add the white space to the left
-    for (let k = 1; k <= (rows - i); k++) {
-      str += ' ';
+    for (let k = 1; k <= rows - i; k++) {
+      str += " ";
     }
 
     // Add the '*' for each row
-    for (let j = 0; j != (2 * i - 1); j++) {
-      str += '*';
+    for (let j = 0; j != 2 * i - 1; j++) {
+      str += "*";
     }
 
     // Add the white space to the right
     for (let k = i + 1; k <= rows; k++) {
-      str += ' ';
+      str += " ";
     }
 
     // Print the pyramid pattern for each row
