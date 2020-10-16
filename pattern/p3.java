1aab654ea895a663ce79da6fc221f3fde5bd3bc1
Restyled.io <commits@restyled.io>
2020-10-16 06:35:05 +0000

    Restyled by clang-format

--- pattern/p3.java
+++ pattern/p3.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
-public class p3
-{
-public static void main(String[] args)
-{
-    Scanner in=new Scanner(System.in);
-    int i,n,j,p;
+public class p3 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int i, n, j, p;
     System.out.println("ENTER THE NUMBER");
-    n=in.nextInt();
-    for(i=1;i<=n;i++)
-    {
-        p=n+1-i;
-        for(j=1;j<=n;j++)
-        {
-            if(j>=i)
-            System.out.print(p--);
-            else
-            System.out.print(" ");
-        }
-        System.out.println();
+    n = in.nextInt();
+    for (i = 1; i <= n; i++) {
+      p = n + 1 - i;
+      for (j = 1; j <= n; j++) {
+        if (j >= i)
+          System.out.print(p--);
+        else
+          System.out.print(" ");
+      }
+      System.out.println();
     }
-}
+  }
 }
