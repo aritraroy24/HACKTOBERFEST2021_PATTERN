ac702698e4dc35a915c9c5e9545066d0dc0b63f6
NeethuJojiGeorge <55632660+NeethuJojiGeorge@users.noreply.github.com>
2020-10-23 21:33:05 +0530

    Number Pattern
    A number pattern using python

---
+++ contributors/neethuojigeorge.md
@@ -0,0 +1,7 @@
+---
+Name: Neethu Joji George
+GitHub Username: neethujojigeorge
+Institution: F.C.R.I.T,Vashi
+Photo Title: neethujojigeorge.jpg
+Screenshot Title: neethujojigeorge_numberpattern.png
+---
---
+++ pattern/neethujojigeorge_numberpattern.py
@@ -0,0 +1,9 @@
+num = int(input("Enter no of rows: "))
+for i in range(1,num+1):
+    for j in range(1,num-i+1):
+        print(" ",end="") 
+    for j in range(i,0,-1):
+        print(j,end="")
+    for j in range(2,i+1):
+        print(j,end="")    
+    print()    
