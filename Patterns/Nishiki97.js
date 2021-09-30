39fc03d340d914bb86a7a27abfda09e9b20a726e
Nishiki Asumi Yapa <nishikiyapa1997@gmail.com>
2021-10-01 03:28:44 +0530

    Added a pattern - Nishiki97.ts

---
+++ Patterns/Nishiki97.js
@@ -0,0 +1,20 @@
+//Typescript version for 'aortiz24.py'
+var size = 7;
+var m = 0;
+var i = 0;
+var j = 0;
+var end = "";
+var patternString = "";
+m = 2 * size - 2;
+for (i = 0; i < size; i++) {
+    for (j = 0; j < m; j++) {
+        end = " ";
+    }
+    m = m - 1;
+    for (j = 0; j < i + 1; j++) {
+        patternString = "* ";
+        end = " ";
+    }
+    patternString = patternString + end;
+}
+console.log(patternString);
