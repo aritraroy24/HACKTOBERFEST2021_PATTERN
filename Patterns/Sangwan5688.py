c761b99cc2ad06115975af272d08f00421b162bf
Ankit Sangwan <ankit.sangwan.5688@gmail.com>
2021-09-30 21:35:25 +0530

    Added pattern

---
+++ Patterns/Sangwan5688.py
@@ -0,0 +1,19 @@
+rows = 5
+k = 2 * rows - 2
+for i in range(rows):
+    for _ in range(k):
+        print(" ", end="")
+    k = k - 1
+    print("*", end="")
+    for _ in range(i*2):
+        print(" ", end="")
+    print("*")
+k = rows - 2
+for i in range(rows, -1, -1):
+    for j in range(k, 0, -1):
+        print(" ",end="")
+    k = k + 1
+    print("*", end="")
+    for _ in range((i)*2):
+        print(" ", end="")
+    print("*")
