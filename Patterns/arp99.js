b17a8608dda79df083f9ecabda0aaccdaa3ff5e1
Arpan Mondal <arpan19991025@gmail.com>
2021-10-01 01:57:03 +0530

    Add new pattern in Js

---
+++ Patterns/arp99.js
@@ -0,0 +1,14 @@
+function pattern(n){
+    let patternStr = "";
+    for (let i = 0; i < n; i++) {
+        patternStr += "*".repeat(i)
+        patternStr += "\n";
+    }
+    for (let i = 0; i < n; i++) {
+        patternStr += "*".repeat(n-i);
+        patternStr += "\n";
+    }
+    console.log(patternStr);
+}
+
+pattern(5)
