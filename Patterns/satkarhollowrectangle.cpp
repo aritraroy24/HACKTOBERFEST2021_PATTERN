c983371e4f5518330b099589af59b56da5c9931d
Satkar Acharya <81009537+satkar2001@users.noreply.github.com>
2021-10-01 00:31:20 +0530

    added hollow rectangle code

---
+++ Patterns/satkarhollowrectangle.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_rectangle(int n, int m)
+{
+    int i, j;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= m; j++)
+        {
+            if (i == 1 || i == n ||
+                j == 1 || j == m)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+
+int main()
+{
+    int rows = 6, columns = 20;
+    print_rectangle(rows, columns);
+    return 0;
+}
