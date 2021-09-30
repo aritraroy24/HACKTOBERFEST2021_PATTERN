d4a9f5fc680a0cae59d3e8576e66b978800a7f77
Kunal jain <72144717+kunaljainwin@users.noreply.github.com>
2021-10-01 00:49:54 +0530

    kunaljainwin.cpp

---
+++ Patterns/kunaljainwin.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+ 	string s="Thank You";
+ 	
+ 	for(int j=0;j<9;j++){
+ 		for(int i=j;i<9;i++){
+ 		cout<<s[i]<<" ";
+	 }
+	 cout<<endl;
+	 }
+ }
