6ae5f662338c665e9af442044b569e662f52fe38
Devashish Rai <devashish.rai.cse20@itbhu.ac.in>
2021-10-01 21:28:05 +0530

    Add Pattern.

---
+++ Patterns/raideva.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+
+    int n = 5;
+    int len = n*2 - 1;
+    for(int i=0;i<len;i++){
+        for(int j=0;j<len;j++){
+            int min = i < j ? i : j;
+            min = min < len-i ? min : len-i-1;
+            min = min < len-j-1 ? min : len-j-1;
+            printf("%d ", n-min);
+        }
+        printf("\n");
+    }
+    return 0;
+}
