cd6d8ed77375263aea7eb46d8d1de90e3966bdc9
aymansharma <88264263+aymansharma@users.noreply.github.com>
2021-10-01 17:10:43 +0530

    Create aymansharma.py

---
+++ Patterns/aymansharma.py
@@ -0,0 +1,27 @@
+rows = 5
+i = 0
+while i <= rows - 1:
+    j = 0
+    while j < i:
+        # display space
+        print('', end=' ')
+        j += 1
+    k = i
+    while k <= rows - 1:
+        print('*', end=' ')
+        k += 1
+    print()
+    i += 1
+
+i = rows - 1
+while i >= 0:
+    j = 0
+    while j < i:
+        print('', end=' ')
+        j += 1
+    k = i
+    while k <= rows - 1:
+        print('*', end=' ')
+        k += 1
+    print('')
+    i -= 1
