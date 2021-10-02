69e52fbfd10735c34c27896a5d365a80655c3c53
Shuvo <38564994+0shuvo0@users.noreply.github.com>
2021-10-02 10:07:35 +0600

    Create hollowSquare.js

---
+++ Patterns/hollowSquare.js
@@ -0,0 +1,23 @@
+let n = 5; // row or column count
+// defining an empty string
+let string = "";
+
+for(let i = 0; i < n; i++) { // external loop
+  for(let j = 0; j < n; j++) { // internal loop
+    if(i === 0 || i === n - 1) {
+      string += "*";
+    }
+    else {
+      if(j === 0 || j === n - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+  }
+  // newline after each row
+  string += "\n";
+}
+// printing the string
+console.log(string);
