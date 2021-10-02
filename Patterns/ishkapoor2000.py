154362bd35bd7528dae13889022f522fc175f787
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2021-10-02 20:29:52 +0530

    Added Functions and Final Code

--- Patterns/ishkapoor2000.py
+++ Patterns/ishkapoor2000.py
@@ -1,10 +1,92 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 2 05:00:00 2021
+Created on Sat Oct  2 10:00:00 2021
 
 @author: ISH KAPOOR
 """
 
-"""
-Complex Pattern Desgns in Python
-"""
+class Pattern:
+
+    def __init__(self, rows):
+        self.rows = rows
+
+
+    def hollowDiamond(self):
+
+        for i in range(self.rows):
+
+            for _ in range(i+1, self.rows+1): print("*", end=" ")
+            for _ in range(i):
+                print(" ", end=" ")
+                print(" ", end=" ")
+            for _ in range(i+1, self.rows+1): print("*", end=" ")
+            if i!=(self.rows-1): print("")
+
+        for i in range(self.rows+1):
+            for _ in range(i): print("*", end=" ")
+            for _ in range(i+1, self.rows+1):
+                print(" ", end=" ")
+                print(" ", end=" ")
+            for _ in range(i): print("*", end=" ")
+            print("")
+
+
+    def cosmicStar(self):
+
+        columnDigit = 2*(self.rows)-5
+
+        for rowItr in range(self.rows):
+            for columnItr in range(columnDigit):
+                if rowItr==2 or rowItr==(self.rows-3) or rowItr+columnItr==(columnDigit//2) or columnItr-rowItr==(columnDigit//2) or (rowItr-columnItr)==2 or (columnItr+rowItr)==(columnDigit+1):
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print("")
+
+
+    def diamondHeart(self):
+
+        for i in range(self.rows//2):
+
+            for j in range((self.rows//2)-i-1):
+                print(" ", end="")
+            for j in range(i+1):
+                print("* ", end="")
+
+            if self.rows%2 == 0: 
+               for j in range(2*((self.rows//2)-i-1)):
+                   print(" ", end="")
+            else:
+                for j in range(2*((self.rows//2)-i-1)+2):
+                   print(" ", end="")
+
+            for j in range(i+1):
+                print("* ", end="")
+
+            print("")
+
+
+        for i in range(self.rows, 0, -1):
+
+            for j in range(self.rows-i):
+                print(" ", end="")
+            for j in range(i, 0, -1):
+                print("* ", end="")
+
+            print("")
+
+pattern = Pattern(15)
+patternInput = int(input("--*--Enter the Pattern-*-\n[1] - Diamond\n[2] - Heart\n[3] - Star\n[0] - QUIT\n>>> "))
+
+if (patternInput <= 3):
+
+    if patternInput == 1:
+        pattern.hollowDiamond()
+    elif patternInput == 2:
+        pattern.diamondHeart()
+    elif patternInput == 3:
+        pattern.cosmicStar()
+    else:
+        print("Thankyou! Visit & Follow: https://github.com/ishkapoor2000/")
+else:
+    print("Invalid Input")
