af5642a8fabf446a616fdeb02be81725e997e19a
AdityaWadkar <67093170+AdityaWadkar@users.noreply.github.com>
2021-10-02 19:40:46 +0530

    Create new pattern using python
    The following program simple draws one inverted rectangle and 2 semi circle which looks like heart shape

---
+++ Patterns/shape_heart.py
@@ -0,0 +1,9 @@
+#The following program simple draws one inverted rectangle and 2 semi circle which looks like heart shape
+
+for row in range(6):
+    for col in range(7):
+        if(row==0 and col %3==1)or(row==1 and col%3==0)or(row-col==2)or(row + col==8):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
