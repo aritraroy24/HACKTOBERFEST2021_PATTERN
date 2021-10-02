96854bb76fae70daf935179410c1889aa06ff970
DEEPANSHU <56041032+Deepanshu276@users.noreply.github.com>
2021-10-02 13:14:14 +0530

    Add files via upload

---
+++ Patterns/pattern.java
@@ -0,0 +1,22 @@
+package lab1;
+public class pattern
+{
+	public static void main(String[] args) 
+	{
+    		int i,j,l,k;
+		for (i=1; i<=3;i++) 
+		{
+      			for (j=0;j<=3-i;j++) {
+        			System.out.print(" ");
+                  }
+			for (k=1;k<i;k++){
+				System.out.print(k);
+            }
+			for (l=i;l>=1;l--)
+            {
+				 System.out.print(l);
+            }
+		    System.out.println();
+    		}
+  	}
+}
