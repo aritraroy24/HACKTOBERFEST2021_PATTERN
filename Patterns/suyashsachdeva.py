88ae0d9dfb86e3539df0505e6c87ff1ace8e2d12
Suyash Sachdeva <77083945+suyashsachdeva@users.noreply.github.com>
2021-10-02 08:24:21 +0530

    Add files via upload

---
+++ Patterns/suyashsachdeva.py
@@ -0,0 +1,19 @@
+rows = 4
+k = 2 * rows - 2
+for i in range(rows):
+    for _ in range(k):
+        print(" ", end="")
+    k = k - 1
+    print("/", end="")
+    for _ in range(i*2):
+        print(" ", end="")
+    print("\\")
+k = rows-1
+for i in range(rows-1, -1, -1):
+    for j in range(k, 0, -1):
+        print(" ",end="")
+    k = k + 1
+    print("\\", end="")
+    for _ in range((i)*2):
+        print(" ", end="")
+    print("/")
