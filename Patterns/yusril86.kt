d042f9788fd57573b35d05b17afbc778a9e3e592
yusril86 <yusril886@gmail.com>
2021-10-02 10:12:49 +0800

    add pattern

---
+++ Patterns/yusril86.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    /*
+    Triangle half
+     *
+     **
+     ***
+     ****
+     ******
+
+     */
+
+
+    val numStart: Int = 5
+
+    for (i in 1..numStart){
+        for (j in 1..i){
+            print("*")
+        }
+        println("")
+    }
+}
