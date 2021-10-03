97674e2fb1f3bc2216ba60017cbfc37ba417cba3
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2021-10-03 08:49:45 +0530

    feat: added a new pattern

---
+++ Patterns/dawksh.tsx
@@ -0,0 +1,22 @@
+
+let size: number = 7;
+let m: number = 0;
+let i: number = 0;
+let j: number = 0;
+let end: string = "";
+let patternString: string = "";
+
+m = 2 * size - 2;
+
+for (i = 0; i < size; i++) {
+  for (j = 0; j < m; j++) {
+    end = " ";
+  }
+  m = m - 1;
+  for (j = 0; j < i + 1; j++) {
+    patternString = "* ";
+    end = " ";
+  }
+  patternString = patternString + end;
+}
+console.log(patternString);
