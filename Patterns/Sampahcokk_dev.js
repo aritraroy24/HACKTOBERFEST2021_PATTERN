33dc1bf251f472f0824fd81efdc3921dda876327
Sampahcokk-dev <yudaplzhacker@gmail.com>
2021-10-03 22:24:17 +0800

    Right pascal star pattern javascript

---
+++ Patterns/Sampahcokk_dev.js
@@ -0,0 +1,15 @@
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+for (let i = 1; i <= n - 1; i++) {
+  for (let j = 0; j < n - i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
