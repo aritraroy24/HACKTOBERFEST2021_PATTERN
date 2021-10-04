47d3a2a37273f2cd25fed72f5d001199f90a90ad
swami-raj <76578276+swami-raj@users.noreply.github.com>
2021-10-04 11:30:27 +0530

    creat within_a_range.cpp

---
+++ within_a_range.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x,y;
+    cin>>n>>x>>y;
+    int arr[n];
+    for (int  i = 0; i < arr[i]; i++)
+    {
+        if ((x<n)&&(y>n)||(x<=y))
+        {
+            cout<<"Yes"<<endl;
+
+        }else{
+            cout<<"No"<<endl;
+        }
+        
+    }
+    
+}
