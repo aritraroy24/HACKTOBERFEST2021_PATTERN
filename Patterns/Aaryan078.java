eb56e01865a92d54cba23ae169daae1ae1e607e2
aaryan078 <67172075+aaryan078@users.noreply.github.com>
2021-10-17 09:41:16 +0530

    Create Aaryan078.java

---
+++ Patterns/Aaryan078.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;  
+public class Aaryan078
+{  
+public static void main(String[] args)  
+{  
+int i, j, k, n;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the number of rows you want to print: ");  
+n = sc.nextInt();              
+for (i= 0; i<= n-1 ; i++)  
+{  
+for (j=0; j<i; j++)  
+{  
+System.out.print(" ");  
+}  
+for (k=i; k<=n-1; k++)   
+{   
+System.out.print("*" + " ");   
+}   
+System.out.println("");   
+}   
+for (i= n-1; i>= 0; i--)  
+{  
+for (j=0; j<i; j++)  
+{  
+System.out.print(" ");  
+}  
+for (k=i; k<=n-1; k++)  
+{  
+System.out.print("*" + " ");  
+}  
+System.out.println("");  
+}  
+sc.close();  
+}  
+}  
