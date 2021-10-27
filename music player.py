12222197d592827e7fff687526072463165d1bee
prashant kumar <47036071+pkkushagra@users.noreply.github.com>
2021-10-27 09:20:57 +0530

    Create music player.py

---
+++ music player.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+
+class MusicPlayer:
+    def __init__(self, window ):
+        window.geometry('320x100'); window.title('Iris Player'); window.resizable(0,0)
+        Load = Button(window, text = 'Load',  width = 10, font = ('Times', 10), command = self.load)
+        Play = Button(window, text = 'Play',  width = 10,font = ('Times', 10), command = self.play)
+        Pause = Button(window,text = 'Pause',  width = 10, font = ('Times', 10), command = self.pause)
+        Stop = Button(window ,text = 'Stop',  width = 10, font = ('Times', 10), command = self.stop)
+        Load.place(x=0,y=20);Play.place(x=110,y=20);Pause.place(x=220,y=20);Stop.place(x=110,y=60) 
+        self.music_file = False
+        self.playing_state = False
+    def load(self):
+        self.music_file = filedialog.askopenfilename()
+    def play(self):
+        if self.music_file:
+            mixer.init()
+            mixer.music.load(self.music_file)
+            mixer.music.play()
+    def pause(self):
+        if not self.playing_state:
+            mixer.music.pause()
+            self.playing_state=True
+        else:
+            mixer.music.unpause()
+            self.playing_state = False
+    def stop(self):
+        mixer.music.stop()
+root = Tk()
+app= MusicPlayer(root)
+root.mainloop()
+© 2021 GitHub, Inc.
+Terms
