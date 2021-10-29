7f2b5d516c8c09786d524cc037182c45912d22d4
Pasindu Vishmika <pasindu.vishmika18@gmail.com>
2021-10-29 18:20:53 +0530

    pyramid

---
+++ Patterns/right side rotated pyramid.py
@@ -0,0 +1,10 @@
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
+
+for i in range(rows, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print("\r")
