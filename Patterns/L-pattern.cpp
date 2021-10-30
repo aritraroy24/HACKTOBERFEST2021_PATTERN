7ecdb000db68ffd2858d1ad636c2e9ea89dbd063
Pratham Aditya Salhotra <66534075+PrathamAditya@users.noreply.github.com>
2021-10-30 21:29:25 +0530

    L pattern
    this pattern is made using c++ language ..

---
+++ Patterns/L-pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << "LLL" << std::endl;
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << "LLLLLLLLLLLLL" << std::endl;
+    }
+}
